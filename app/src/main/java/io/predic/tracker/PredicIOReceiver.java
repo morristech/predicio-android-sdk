67b08f192840dcca4f2223b0ce85a5166f6ecc1e
camillepredicio <camille.chelpi@hotmail.fr>
2018-05-25 16:46:33 +0200

    Apps fix

--- app/src/main/java/io/predic/tracker/PredicIOReceiver.java
+++ app/src/main/java/io/predic/tracker/PredicIOReceiver.java
@@ -33,9 +33,7 @@ public class PredicIOReceiver extends BroadcastReceiver {
 
         if (intent.getAction().equals(PredicIO.ACTION_TRACK_APPS)) {
             JSONObject obj = PredicIO.getInstance().getJSONObjectApps(context);
-            if (obj != null) {
-                PredicIO.getInstance().sendHttpAppsRequest(obj);
-            }
+            if (obj != null) PredicIO.getInstance().sendHttpAppsRequest(obj);
         } else if (intent.getAction().equals(PredicIO.ACTION_TRACK_IDENTITY)) {
             PredicIO.getInstance().sendHttpIdentityRequest();
         } else if (intent.getAction().equals(PredicIO.ACTION_TRACK_LOCATION)) {
